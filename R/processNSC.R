824ac4643cef88b2a2602eb9be85de0c8c3d89f2
Tim Tito Rademacher <ttr22@cam.ac.uk>
2019-02-14 11:44:48 -0700

    Change linear fit for the calibration curve and include correction due to sample blanks as well as correction due to potato starch recovery standard.

--- R/processNSC.R
+++ R/processNSC.R
@@ -7,11 +7,16 @@
 #' @param forceIntercept Logical variable describing whether to force the calibration curve intercept through 0 or not.
 #' @return The processed data, results of the anyalsis and a summary.
 #' @export
-processNSCs <- function (rawData, cvLimitSample = 0.25, cvLimitTube = 0.05, forceIntercept = FALSE) {
+processNSCs <- function (rawData,
+                         cvLimitSample = 0.25,
+                         cvLimitTube = 0.10,
+                         forceIntercept = FALSE,
+                         maxStarchRecoveryFraction = 0.9) {
 
-  # Calculate the sample weight [g]
+  # Calculate the sample weight [g] and convert to [mg]
   #--------------------------------------------------------------------------------------
-  rawData [['MassSample']] <- (rawData [['MassOfTubeAndSample']] - rawData [['MassOfEmptyTube']]) * 1e3
+  rawData [['MassSample']] <- (rawData [['MassOfTubeAndSample']] -
+                               rawData [['MassOfEmptyTube']]) * 1e3
 
   # Get indices for samples with negative weight
   #--------------------------------------------------------------------------------------
@@ -24,11 +29,12 @@ processNSCs <- function (rawData, cvLimitSample = 0.25, cvLimitTube = 0.05, forc
         rawData [['SampleID']] [i] == 'TB') rawData [['MassSample']] [i] <- 0.0
   }
 
-
   # Error for non-blanks with negativ weight.
   #--------------------------------------------------------------------------------------
   if (length (which (rawData [['MassSample']] < 0.0)) > 0) {
-    if (which (rawData [['MassSample']] < 0.0)) stop ('Error: There is a non-blank sample with a negative weight.')
+    if (which (rawData [['MassSample']] < 0.0)) {
+      stop ('Error: There is a non-blank sample with a negative weight.')
+    }
   }
 
   # Create two new colums with the average absorbance at 490 and 525 nm
@@ -37,32 +43,38 @@ processNSCs <- function (rawData, cvLimitSample = 0.25, cvLimitTube = 0.05, forc
   absorbances525 <- cbind (rawData [['Absorbance525_1']], rawData [['Absorbance525_2']])
   rawData [['MeanAbsorbance490']] <- rowMeans (absorbances490)
   rawData [['MeanAbsorbance525']] <- rowMeans (absorbances525)
+  rawData [['CorrectedMeanAbsorbance490']] <- rawData [['MeanAbsorbance490']] -
+                                              rawData [['Absorbance490_Blank']]
 
   # Calculate the within-sample coefficient of variation
   #--------------------------------------------------------------------------------------
   rawData [['SDAbsorbance490']] <- apply (absorbances490, 1, sd)
   rawData [['SDAbsorbance525']] <- apply (absorbances525, 1, sd)
-  rawData [['CVAbsorbance490']] <- rawData [['SDAbsorbance490']] / rawData [['MeanAbsorbance490']]
-  rawData [['CVAbsorbance525']] <- rawData [['SDAbsorbance525']] / rawData [['MeanAbsorbance525']]
+  rawData [['CVAbsorbance490']] <- rawData [['SDAbsorbance490']] /
+                                   rawData [['MeanAbsorbance490']]
+  rawData [['CVAbsorbance525']] <- rawData [['SDAbsorbance525']] /
+                                   rawData [['MeanAbsorbance525']]
 
   # Flag samples with high CV for re-measuring
   #--------------------------------------------------------------------------------------
   rawData [['HighCV']] <- 'N'
   rawData [['HighCV']] [rawData [["CVAbsorbance490"]] > cvLimitTube] <- 'Sugar'
-  rawData [['HighCV']] [rawData [["CVAbsorbance525"]] > cvLimitTube & rawData [['HighCV']] == 'N'] <- 'Starch'
-  rawData [['HighCV']] [rawData [["CVAbsorbance525"]] > cvLimitTube & rawData [['HighCV']] == 'Sugar'] <- 'Sugar and starch'
+  rawData [['HighCV']] [rawData [["CVAbsorbance525"]] > cvLimitTube &
+                        rawData [['HighCV']] == 'N'] <- 'Starch'
+  rawData [['HighCV']] [rawData [["CVAbsorbance525"]] > cvLimitTube &
+                        rawData [['HighCV']] == 'Sugar'] <- 'Sugar and starch'
 
   # Extract blanks with solution
   #--------------------------------------------------------------------------------------
-  blanks <- rawData [rawData [['SampleID']] == 'B', ]
+  #blanks <- rawData [rawData [['SampleID']] == 'B', ]
 
   # Extract blanks with only empty tubes
   #--------------------------------------------------------------------------------------
-  tubeBlanks <- rawData [rawData [['SampleID']] == 'TB', ]
+  #tubeBlanks <- rawData [rawData [['SampleID']] == 'TB', ]
 
   # Extract Lab Control Standards (LCS)
   #--------------------------------------------------------------------------------------
-  labControlStandards <- rawData [substr (rawData [['SampleID']], 1, 3) == 'LCS', ]
+  #labControlStandards <- rawData [substr (rawData [['SampleID']], 1, 3) == 'LCS', ]
 
   # Compile a list of batches
   #--------------------------------------------------------------------------------------
@@ -72,31 +84,55 @@ processNSCs <- function (rawData, cvLimitSample = 0.25, cvLimitTube = 0.05, forc
   #--------------------------------------------------------------------------------------
   for (batch in batches) {
     for (NSC in c ('sugar','starch')) {
-      condition <- substr (rawData [['SampleID']], 1, 3) == 'REF' & rawData [['BatchID']] == batch
+
+      # Get absorbances for reference values to create a calibration curve for each batch
+      #----------------------------------------------------------------------------------
+      condition <- substr (rawData [['SampleID']], 1, 3) == 'REF' &
+                           rawData [['BatchID']] == batch
       referenceValues <- rawData [condition, ]
-      concentrations <- substr (rawData [['SampleID']] [condition], 4, nchar (rawData [['SampleID']] [condition]))
+
+      # Get reference solution concentrations
+      #----------------------------------------------------------------------------------
+      concentrations <- substr (rawData [['SampleID']] [condition],
+                                4,
+                                nchar (rawData [['SampleID']] [condition]))
+
+      # Set reference solution concentrations depending on whether we are calibrating
+      # starch or sugar
+      #----------------------------------------------------------------------------------
       if (NSC == 'sugar') {
         concentrations [concentrations == '100/200'] <- 100.0 # 100 for sugar
       } else if (NSC == 'starch'){
         concentrations [concentrations == '100/200'] <- 200.0 # 200 for starrch
+
+        # Get the batch's mean absorbance at 525nm for tube blanks
+        #--------------------------------------------------------------------------------
+        batchTBAbsorbance <- rawData [['MeanAbsorbance525']] [rawData [['SampleID']] == 'TB']
+
+        # Check that TB absorbance is not larger than the sample, otherwise set flag
+        #--------------------------------------------------------------------------------
+        #if (batchTBAbsorbance > ) {}
+
+        # Correct reference value for tube blank absorbance
+        #--------------------------------------------------------------------------------
+        rawData [['CorrectedMeanAbsorbance525']] [rawData [['BatchID']] == batch] <- rawData [['MeanAbsorbance525']] -
+                                                                                     batchTBAbsorbance
       }
       concentrations <- as.numeric (concentrations)
 
       # Get the slope and intercept
       #----------------------------------------------------------------------------------
       if (forceIntercept) { # Get slope for intercepts forced through zero
-        if (NSC == 'sugar') {
-          fitSugar  <- lm (referenceValues [['MeanAbsorbance490']] -
-                           referenceValues [['Absorbance490_Blank']] ~ 0 + concentrations)
+        if (NSC == 'sugar') { # sugar <- slope * absorbance + intercept
+          fitSugar  <- lm (concentrations ~ 0 + referenceValues [['CorrectedMeanAbsorbance490']])
         } else if (NSC == 'starch') {
-          fitStarch <- lm (referenceValues [['MeanAbsorbance525']] ~ 0 + concentrations)
+          fitStarch <- lm (concentrations ~ 0 + referenceValues [['CorrectedMeanAbsorbance525']])
         }
       } else { # Get intercept and slope
         if (NSC == 'sugar') {
-          fitSugar  <- lm (referenceValues [['MeanAbsorbance490']] -
-                           referenceValues [['Absorbance490_Blank']] ~ concentrations)
+          fitSugar  <- lm (concentrations ~ referenceValues [['CorrectedMeanAbsorbance490']])
         } else if (NSC == 'starch') {
-          fitStarch <- lm (referenceValues [['MeanAbsorbance525']] ~ concentrations)
+          fitStarch <- lm (concentrations ~ referenceValues [['CorrectedMeanAbsorbance525']])
         }
       }
 
@@ -122,35 +158,62 @@ processNSCs <- function (rawData, cvLimitSample = 0.25, cvLimitTube = 0.05, forc
     }
   }
 
-  # Determine concentrations from absorbance values for sugar
+  # Determine concentrations from absorbance values for sugar # Call it a concentrations [mg ml-1]
   #--------------------------------------------------------------------------------------
-  rawData [['MassSugarM']] <- (rawData [['MeanAbsorbance490']] - rawData [['InterceptSugar']]) /
-                               rawData [['SlopeSugar']]
-  rawData [['MassSugarB']] <- (rawData [['Absorbance490_Blank']] - rawData [['InterceptSugar']]) /
-                               rawData [['SlopeSugar']]
+  rawData [['ConcentrationSugar']] <- rawData [['CorrectedMeanAbsorbance490']] *
+                                      rawData [['SlopeSugar']] +
+                                      rawData [['InterceptSugar']]
 
-
-  # Correct sugar amount [mg] for background signal using no phenol
+  # Correct sugar amount [mg ml-1] for background signal using no phenol
   #--------------------------------------------------------------------------------------
-  rawData [['MassSugar']] <- (rawData [['MassSugarM']] - rawData [['MassSugarB']]) *
-                              rawData [['DilutionFactorSugar']] * rawData [['VolumeSugar']]
+  rawData [['MassSugar']] <- rawData [['ConcentrationSugar']] *
+                             rawData [['DilutionFactorSugar']] *
+                             rawData [['VolumeSugar']]
 
-  # Determine amount of strach [mg] from absorbance
+  # Determine concentration of strach [mg ml-1] from absorbance
   #--------------------------------------------------------------------------------------
-  rawData [['MassStarch']] <- (rawData [['MeanAbsorbance525']] - rawData [['InterceptStarch']]) /
-                               rawData [['SlopeStarch']]
-  rawData [['MassStarch']] <- rawData [['MassStarch']] * rawData [['DilutionFactorStarch']] *
+  rawData [['ConcentrationStarch']] <- rawData [['CorrectedMeanAbsorbance525']] *
+                                       rawData [['SlopeStarch']] +
+                                       rawData [['InterceptStarch']]
+  rawData [['MassStarch']] <- rawData [['ConcentrationStarch']] *
+                              rawData [['DilutionFactorStarch']] *
                               rawData [['VolumeStarch']]
+
+  # Calcualte starch recovery
+  #--------------------------------------------------------------------------------------
+  for (batch in batches) {
+    # Get absorbances for potato starch for each batch
+    #----------------------------------------------------------------------------------
+    condition <- substr (rawData [['SampleID']], 1, 3) == 'LCS potato' &
+                         rawData [['BatchID']] == batch
+    LCSPotato <- rawData [condition, ]
+    meanPotatoMassRecovered <- mean (LCSPotato [['MassStarch']])
+    meanPotatoMass <- mean (LCSPotato [['MassSample']])
+    meanRecoveryPer <- meanPotatoMassRecovered /
+                      (meanPotatoMass * maxStarchRecoveryFraction) * 100.0
+
+    # Set batch's mean starch recovery rate
+    #----------------------------------------------------------------------------------
+    rawData [['MeanStarchRecovery']] [rawData [['BatchID']] == batch] <- meanRecoveryPer
+  }
+
+  # Correct all samples for the mean starch recovery percentage of each batch
+  #------------------------------------------------------------------------------------
+  rawData [['CorrectedMassStarch']] <- rawData [['MassStarch']] /
+                                      (rawData [['MeanStarchRecovery']] / 100.0)
 
   # Convert mass to concentrations [mg g-1]
   #--------------------------------------------------------------------------------------
-  rawData [['ConcentrationSugarMgG']]  <- rawData [['MassSugar']]  / rawData [['MassSample']]
-  rawData [['ConcentrationStarchMgG']] <- rawData [['MassStarch']] / rawData [['MassSample']]
+  rawData [['ConcentrationSugarMgG']]  <- rawData [['MassSugar']] /
+                                          rawData [['MassSample']]
+  rawData [['ConcentrationStarchMgG']] <- rawData [['CorrectedMassStarch']] /
+                                          rawData [['MassSample']]
 
   # Convert concentrations from [mg g-1] to [% dry weight]
   #--------------------------------------------------------------------------------------
   rawData [['ConcentrationSugarPerDW']]  <- rawData [['ConcentrationSugarMgG']]  / 10.0
   rawData [['ConcentrationStarchPerDW']] <- rawData [['ConcentrationStarchMgG']] / 10.0
+
 
   # Declare data as processed
   #--------------------------------------------------------------------------------------
@@ -160,3 +223,5 @@ processNSCs <- function (rawData, cvLimitSample = 0.25, cvLimitTube = 0.05, forc
   #--------------------------------------------------------------------------------------
   return (processedData)
 }
+# To-do-list:
+# TTR Correct starch concentration using the TB and percentage of extracted potato starch
