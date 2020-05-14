22b108c5082ec2dab30d19430c8cdeaafdfae29f
TTRademacher <rademacher.tim@gmail.com>
2020-05-14 10:16:47 -0700

    update plotControlStandards () function to include starch values for the Harvard Forest Oak standard.

--- R/plotLabControlStandards.R
+++ R/plotLabControlStandards.R
@@ -14,40 +14,47 @@ plotLabControlStandards <- function (data) {
   #--------------------------------------------------------------------------------------
   for (standard in labControlStandards) {
 
+    # Check whether it is a starch or sugar standard
+    #----------------------------------------------------------------------------------
+    if (standard == 'LCS Oak') {
+      datesSugar  <- data [['DateOfSugarAnalysis']] [data [['SampleID']] == standard]
+      datesStarch <- data [['DateOfStarchAnalysis']] [data [['SampleID']] == standard]
+      concentrationsOfSugarStds <- data [['ConcentrationSugarPerDW']] [data [['SampleID']] == standard]
+      concentrationsOfStarchStds <- data [['ConcentrationStarchPerDW']] [data [['SampleID']] == standard]
+    } else if (standard == 'LCS Potato') {
+      datesStarch <- data [['DateOfStarchAnalysis']] [data [['SampleID']] == standard]
+      concentrationsOfStarchStds <- data [['ConcentrationStarchPerDW']] [data [['SampleID']] == standard]
+    }
+
+    # Check whether this standard is for sugar
+    #----------------------------------------------------------------------------------
+    if (exists ('datesSugar')) {
+
       # Create fileName for the pdf
       #----------------------------------------------------------------------------------
-      fileName <- paste (standard,".png", sep = "")
+      fileName <- paste (standard,"_SolubleSugar.png", sep = "")
 
       # Open the png
       #----------------------------------------------------------------------------------
       png (file = fileName)
 
-      # Check whether it is a starch or sugar standard
+      # Plot the sugar standard over time
       #----------------------------------------------------------------------------------
-      if (standard == 'LCS Oak') {
-        dates <- data [['DateOfSugarAnalysis']] [data [['SampleID']] == standard]
-        concentrationsOfStds <- data [['ConcentrationSugarPerDW']] [data [['SampleID']] == standard]
-      } else if (standard == 'LCS Potato') {
-        dates <- data [['DateOfStarchAnalysis']] [data [['SampleID']] == standard]
-        concentrationsOfStds <- data [['ConcentrationStarchPerDW']] [data [['SampleID']] == standard]
-      }
-
-      # plot the standard over time
-      #----------------------------------------------------------------------------------
-      plot (x = dates,
-            y = concentrationsOfStds,
+      plot (x = datesSugar,
+            y = concentrationsOfSugarStds,
             main = paste (standard,'over time'),
             xlab = 'time',
-            ylab = 'concentration (% dry weight)',
+            ylab = 'soluble sugar concentration (% dry weight)',
             las = 1,
             col = '#91b9a499',
             pch = 19,
-            ylim  = c (0, 1.5*max (concentrationsOfStds)))
+            ylim  = c (0, 1.5*max (concentrationsOfSugarStds)))
       if (standard == 'LCS Oak') {
+
         # Add morgan's interval of variation
         #--------------------------------------------------------------------------------
-        rect (xleft   = min (dates) - 86400*365,
-              xright  = max (dates) + 86400*365,
+        rect (xleft   = min (datesSugar) - 86400*365,
+              xright  = max (datesSugar) + 86400*365,
               ybottom = 3.59-0.38,
               ytop    = 3.59+0.38,
               col = "#33333333",
@@ -57,8 +64,8 @@ plotLabControlStandards <- function (data) {
 
         # Add jim's interval of variation
         #--------------------------------------------------------------------------------
-        rect (xleft   = min (dates) - 86400*365,
-              xright  = max (dates) + 86400*365,
+        rect (xleft   = min (datesSugar) - 86400*365,
+              xright  = max (datesSugar) + 86400*365,
               ybottom = 2.45-0.21,
               ytop    = 2.45+0.21,
               col = "#91b9a444",
@@ -70,8 +77,8 @@ plotLabControlStandards <- function (data) {
 
       # Add the mean and standard deviation
       #----------------------------------------------------------------------------------
-      means <- aggregate (concentrationsOfStds, by = list (dates), FUN = mean)
-      sds <- aggregate (concentrationsOfStds, by = list (dates), FUN = sd)
+      means <- aggregate (concentrationsOfSugarStds, by = list (datesSugar), FUN = mean)
+      sds <- aggregate (concentrationsOfSugarStds, by = list (datesSugar), FUN = sd)
       arrows (x0 = means [, 1],
               y0 = means [, 2] - sds [, 2],
               x1 = means [, 1],
@@ -85,21 +92,85 @@ plotLabControlStandards <- function (data) {
               pch = 15,
               col = '#DC143C99')
 
-      # add legend
+      # Add legend
       #----------------------------------------------------------------------------------
       legend (x = min (dates),
-              y = 1.5*max (concentrationsOfStds),
+              y = 1.5*max (concentrationsOfSugarStds),
               legend = c ('mean ± standard deviation','individual measurement'),
               pch = c (15, 19),
               col = c ('#DC143C99','#91b9a499'),
               box.lty = 0,
               bg = 'transparent')
 
-      # close graphics device
+      # Close graphics device
       #----------------------------------------------------------------------------------
       dev.off ()
 
-    } # End the standards loop
+    } # End if sugar exists condition
+
+    # Check whether a starch standard exists
+    #----------------------------------------------------------------------------------
+    if (exists ('datesStarch')) {
+
+      # Create fileName for the png file
+      #----------------------------------------------------------------------------------
+      fileName <- paste (standard,"_Starch.png", sep = "")
+
+      # Open the png file
+      #----------------------------------------------------------------------------------
+      png (file = fileName)
+
+      # Plot the starch standard over time
+      #----------------------------------------------------------------------------------
+      plot (x = datesStarch,
+            y = concentrationsOfStarchStds,
+            main = paste (standard,'over time'),
+            xlab = 'time',
+            ylab = 'soluble sugar concentration (% dry weight)',
+            las = 1,
+            col = '#91b9a499',
+            pch = 19,
+            ylim  = c (0, 1.5*max (concentrationsOfStarchStds)))
+      if (standard == 'LCS Potato') {
+        abline (h = 100,
+                col = '#66666666',
+                lty = 1)
+      }
+
+      # Add the mean and standard deviation
+      #----------------------------------------------------------------------------------
+      means <- aggregate (concentrationsOfStarchStds, by = list (datesStarch), FUN = mean)
+      sds <- aggregate (concentrationsOfStarchStds, by = list (datesStarch), FUN = sd)
+      arrows (x0 = means [, 1],
+              y0 = means [, 2] - sds [, 2],
+              x1 = means [, 1],
+              y1 = means [, 2] + sds [, 2],
+              col = '#DC143C99',
+              length = 0.05,
+              angle = 90,
+              code = 3)
+      points (x = means [, 1],
+              y = means [, 2],
+              pch = 15,
+              col = '#DC143C99')
+
+      # Add legend
+      #----------------------------------------------------------------------------------
+      legend (x = min (datesStarch),
+              y = 1.5*max (concentrationsOfStarchStds),
+              legend = c ('mean ± standard deviation','individual measurement'),
+              pch = c (15, 19),
+              col = c ('#DC143C99','#91b9a499'),
+              box.lty = 0,
+              bg = 'transparent')
+
+      # Close graphics device
+      #----------------------------------------------------------------------------------
+      dev.off ()
+
+    } # End if starch exists condition
+
+  } # End the standards loop
 
   # Return zero exit status, if it ran smoothly
   #--------------------------------------------------------------------------------------
